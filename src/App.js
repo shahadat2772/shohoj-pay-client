1060a5bdfdf21513a0b8cb032be7bb09174a673b
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-24 17:32:21 +0600

    login setup complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import Home from "./Components/Pages/Home/Home";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 import Footer from "./Components/Shared/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Components/Pages/Authentication/Login/Login";
+import NotFound from "./Components/Shared/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
       <Footer></Footer>
     </div>
   );
