19dcc56fa89856974fe11d8f12f2c84f77424343
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-26 12:21:20 +0600

    dashboard implement complement

--- src/Components/Pages/Dashboard/Dashboard.js
+++ src/Components/Pages/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { PieChart, Pie, Cell, Tooltip, Bar, Legend } from "recharts";
 import "./Dashboard.css";
-const fakeUserData = [
+// USER TRANSACTION FAKE DATA
+const fakeTransaction = [
   {
     _id: 1,
     img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
@@ -44,35 +46,77 @@ const fakeUserData = [
     money: 713,
   },
 ];
+const COLORS = ["#000", "#414CDA", "#23E792", "#FF8042"];
+// FAKE SAVINGS DATA
+const monthSavings = [
+  {
+    _id: 1,
+    img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
+    month: "January",
+    year: "2022",
+    money: 713,
+  },
+  {
+    _id: 2,
+    img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
+    month: "April",
+    year: "2022",
+    money: 45136,
+  },
+  {
+    _id: 3,
+    img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
+    month: "July",
+    year: "2022",
+    money: 4548,
+  },
+];
+// FAKE DATA
+
+const data = [
+  { name: "January", value: 7541, email: "ahsdf@gmail.com" },
+  { name: "April", value: 6574, email: "ahsdf@gmail.com" },
+  { name: "July", value: 5465, email: "ahsdf@gmail.com" },
+];
+// FIND TODAY DATE MONTH YEAR
+let dateObj = new Date();
+let shortMonth = dateObj.toLocaleString("default", { month: "long" });
+let getDate =
+  dateObj.getUTCDate() + " " + shortMonth + "," + dateObj.getUTCFullYear();
+
+// WELCOME DASHBOARD SECTION
 const Dashboard = () => {
   return (
     <div className="container mx-auto mt-24 lg:mt-28 lg:px-10 py-10">
+      {/* START USER INFORMATION AND TRANSACTION */}
       <div className="lg:flex">
         <div>
           <div class="md:mx-10 lg:mx-0 card lg:w-96 shadow-xl bg-primary text-white py-5 rounded card-1-bg">
             <div class="card-body">
-              <h1 className="text-left text-4xl font-bold">User Name</h1>
+              <h1 className="text-left text-4xl font-bold">Minhajul Alam</h1>
               <h5 class="text-left">UserEmail@gmail.com</h5>
 
-              <div class="card-actions justify-start text-3xl">
-                <h1>$5464</h1>
+              <div class="text-left">
+                <h4 className="font-bold">Total Balance</h4>
+                <h1 className="text-3xl">$5464</h1>
               </div>
             </div>
           </div>
+          {/* START RESECT TRANSACTION  */}
           <div className="mt-10 text-left md:mx-10 mx-5 lg:mx-0">
             <h3 className="font-bold text-xl border-b border-black pb-1">
               Recent Transaction
             </h3>
             <div className="mt-8">
               <ul>
-                {fakeUserData.map((transAction) => (
+                {fakeTransaction.slice(0, 4).map((transAction) => (
                   <li
                     className="flex items-center my-4 p-3 rounded-lg shadow-lg"
                     key={transAction._id}
                   >
                     <div class="avatar">
                       <div class="w-16 rounded-full ">
-                        <img src={transAction.img} />
+                        <img src={transAction.img} alt="User Image" />
                       </div>
                     </div>
                     <div className="ml-5 flex items-center justify-between w-full">
@@ -90,10 +134,16 @@ const Dashboard = () => {
                     </div>
                   </li>
                 ))}
+                <div className="text-center">
+                  <button className="btn btn-primary btn-sm mt-5 p-2">
+                    View All Transaction
+                  </button>
+                </div>
               </ul>
             </div>
           </div>
         </div>
+        {/* CARD DIVIDER HORIZONTAL */}
         <div class="divider divider-horizontal divide-black px-9 divider-hidden"></div>
         <div className="w-full">
           <div class="card  shadow-xl rounded bg-image py-8 text-white">
@@ -101,40 +151,40 @@ const Dashboard = () => {
               <h3 className="text-xl">Hi User!</h3>
               <div className="mt-2">
                 <h3 className="font-bold text-xl">
-                  Have a Shohoj Day With Shohoj Pay
+                  Have a Good Day With Shohoj Pay
                 </h3>
-                <h5>Lorem ipsum dolor sit amet consectetur adipisicing.</h5>
+                <h5>Thank You For Staying With Shohoj Pay.</h5>
+                <h5>We Are Always Working To Keep Your Account Safe.</h5>
                 <button className="btn btn-primary mt-4">Button</button>
               </div>
             </div>
           </div>
+          {/* START SAVINGS MONTH */}
           <div className="mt-10 flex">
             <div className="w-3/6">
               <h3 className="font-bold text-xl border-b border-black pb-2">
-                Savings
+                Recent Month Savings
               </h3>
-              <div>
+              <div className="mt-16">
                 <ul>
-                  {fakeUserData.slice(0, 3).map((transAction) => (
+                  {monthSavings.slice(0, 3).map((savings) => (
                     <li
                       className="flex items-center my-4 p-3 rounded-lg shadow-lg"
-                      key={transAction._id}
+                      key={savings._id}
                     >
                       <div class="avatar">
                         <div class="w-16 rounded-full ">
-                          <img src={transAction.img} />
+                          <img src={savings.img} alt="User Image" />
                         </div>
                       </div>
                       <div className="ml-5 flex items-center justify-between w-full">
                         <div>
-                          <h3 className="font-bold text-lg">
-                            {transAction.name}
-                          </h3>
-                          <h5>{transAction.location}</h5>
+                          <h3 className="font-bold text-lg">{savings.month}</h3>
+                          <h5>{savings.year}</h5>
                         </div>
                         <div>
                           <h3 className="text-lg font-bold">
-                            ${transAction.money}
+                            ${savings.money}
                           </h3>
                         </div>
                       </div>
@@ -143,9 +193,40 @@ const Dashboard = () => {
                 </ul>
               </div>
             </div>
-            <div>
-              <h3>Setting</h3>
-              <div></div>
+            {/* START STATISTIC */}
+            <div className="w-3/6 md:ml-8">
+              <div className="px-2">
+                <h3 class="font-bold text-xl pb-2 border-b border-black">
+                  Statistic
+                </h3>
+                <h5 className="font-bold text-right text-xl">{getDate}</h5>
+                <div className="">
+                  {/* <h4 className="font-bold text-2xl">Expense</h4> */}
+                  <div className=" flex justify-center">
+                    <PieChart width={290} height={330}>
+                      <Tooltip />
+                      <Legend style={{ width: "363px" }} />
+                      <Pie
+                        data={data}
+                        cx={120}
+                        cy={200}
+                        innerRadius={65}
+                        outerRadius={78}
+                        fill="#8884d8"
+                        paddingAngle={1}
+                        dataKey="value"
+                      >
+                        {data.map((entry, index) => (
+                          <Cell
+                            key={`cell-${index}`}
+                            fill={COLORS[index % COLORS.length]}
+                          />
+                        ))}
+                      </Pie>
+                    </PieChart>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
