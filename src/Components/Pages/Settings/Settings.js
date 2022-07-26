56a3b8777ef035f46860441d452e2aee3e9b7c34
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-26 10:15:32 +0600

    general settings section added

--- src/Components/Pages/Settings/Settings.js
+++ src/Components/Pages/Settings/Settings.js
@@ -4,12 +4,44 @@ const Settings = () => {
     return (
         <section className='px-5 pt-20 lg:px-20 lg:pb-20 lg:pt-40 lg:flex w-full'>
             {/* right part */}
-            <div className='border border-primary w-full lg:w-1/2 order-2'>
+            <div className='border border-primary w-full lg:w-1/2 order-2 grid grid-cols-1 gap-5 p-5'>
                 {/* user div */}
-                <div></div>
+                <div className=''>
+                    <div className=' '>
+                        <figure className='flex justify-end mb-3'>
+                            <div className='h-32 w-32 bg-blue-200 rounded-full' ></div>
+                        </figure>
+                        <h3 className='text-3xl text-right font-semibold'>User Name</h3>
+                    </div>
+                </div>
 
                 {/* general options section */}
-                <div></div>
+                <div className='rounded-xl p-3 lg:p-10 bg-slate-100'>
+
+                    {/* title div */}
+                    <div className='w-1/2 '>
+                        <h3 className='text-2xl text-left'>General</h3>
+                        <hr></hr>
+                    </div>
+
+                    {/* options container */}
+
+                    <div className='mt-5 grid grid-cols-1 gap-3'>
+
+                        {/* Name */}
+                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                            <input className='input input-text input-secondary' type="text" placeholder='Name' />
+                            <button className='btn max-w-min btn-primary'>change</button>
+                        </div>
+                        {/* postal code */}
+                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                            <input className='input input-number input-secondary' type="number" placeholder=' ' />
+                            <button className='btn max-w-min btn-primary'>change</button>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
 
             {/* left part  */}
