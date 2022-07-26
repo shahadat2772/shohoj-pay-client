9a8bb70732438d68c03ebd94a1f01defa86692a6
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-26 10:36:52 +0600

    divs replaced with form

--- src/Components/Pages/Settings/Settings.js
+++ src/Components/Pages/Settings/Settings.js
@@ -4,7 +4,7 @@ const Settings = () => {
     return (
         <section className='px-5 pt-20 lg:px-20 lg:pb-20 lg:pt-40 lg:flex w-full'>
             {/* right part */}
-            <div className='border border-primary w-full lg:w-1/2 order-2 grid grid-cols-1 gap-5 p-5'>
+            <div className='w-full lg:w-1/2 order-2 grid grid-cols-1 gap-5 p-5'>
                 {/* user div */}
                 <div className=''>
                     <div className=' '>
@@ -29,15 +29,15 @@ const Settings = () => {
                     <div className='mt-5 grid grid-cols-1 gap-3'>
 
                         {/* Name */}
-                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                             <input className='input input-text input-secondary' type="text" placeholder='Name' />
-                            <button className='btn max-w-min btn-primary'>change</button>
-                        </div>
+                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        </form>
                         {/* postal code */}
-                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                             <input className='input input-number input-secondary' type="number" placeholder=' ' />
-                            <button className='btn max-w-min btn-primary'>change</button>
-                        </div>
+                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        </form>
 
                     </div>
 
@@ -45,7 +45,7 @@ const Settings = () => {
             </div>
 
             {/* left part  */}
-            <div className='border border-secondary w-full lg:w-1/2 grid grid-cols-1 gap-5 p-5'>
+            <div className=' w-full lg:w-1/2 grid grid-cols-1 gap-5 p-5'>
                 {/* address section */}
                 <div className='rounded-xl p-3 lg:p-10 bg-slate-100'>
 
@@ -60,15 +60,15 @@ const Settings = () => {
                     <div className='mt-5 grid grid-cols-1 gap-3'>
 
                         {/* address */}
-                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                             <input className='input input-text input-secondary' type="text" placeholder='address' />
-                            <button className='btn max-w-min btn-primary'>change</button>
-                        </div>
+                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        </form>
                         {/* postal code */}
-                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                             <input className='input input-number input-secondary' type="number" placeholder='postal code' />
-                            <button className='btn max-w-min btn-primary'>change</button>
-                        </div>
+                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        </form>
 
                     </div>
 
@@ -87,15 +87,15 @@ const Settings = () => {
                     <div className='mt-5 grid grid-cols-1 gap-3'>
 
                         {/* Email */}
-                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                             <input className='input input-email input-secondary' type="email" placeholder='E-mail Address' />
-                            <button className='btn max-w-min btn-primary'>change</button>
-                        </div>
+                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        </form>
                         {/* phone number */}
-                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
+                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                             <input className='input input-tel input-secondary' type={"tel"} placeholder='Phone Number' />
-                            <button className='btn max-w-min btn-primary'>change</button>
-                        </div>
+                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        </form>
 
                     </div>
                 </div>
@@ -110,18 +110,18 @@ const Settings = () => {
 
                     {/* options container */}
 
-                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 mt-5'>
+                    <form className='grid grid-cols-1 lg:grid-cols-2 gap-3 mt-5'>
                         {/* Current Password */}
                         <input className='input input-password input-secondary' type="password" placeholder='Current Password' />
 
                         <input className='input input-password input-secondary' type="password" placeholder='New Password' />
-                        <button className='btn max-w-min btn-primary'>change</button>
-                    </div>
+                        <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                    </form>
                 </div>
 
                 {/* delete div */}
                 <div className='w-1/2 p-3 lg:p-10 '>
-                    <button className='btn btn-outline btn-error ml-0 mb-3'>Delete Account</button>
+                    <button type="submit" className='btn btn-outline btn-error ml-0 mb-3'>Delete Account</button>
                     <hr></hr>
                 </div>
             </div>
