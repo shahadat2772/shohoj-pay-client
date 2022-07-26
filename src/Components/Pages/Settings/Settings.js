afbddaa4f533d714992d68d0a847bd7f1e336e3b
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-26 18:10:17 +0600

    cards styles updated

--- src/Components/Pages/Settings/Settings.js
+++ src/Components/Pages/Settings/Settings.js
@@ -6,12 +6,12 @@ const Settings = () => {
             {/* right part */}
             <div className='w-full lg:w-1/2 order-2 p-5'>
                 {/* user div */}
-                <div className='p-5'>
+                <div className='py-10'>
                     <div className=' '>
-                        <figure className='flex justify-end mb-3'>
+                        <figure className='flex justify-start mb-3'>
                             <div className='h-32 w-32 bg-secondary bg-opacity-25 rounded-full' ></div>
                         </figure>
-                        <h3 className='text-3xl text-right font-semibold'>User Name</h3>
+                        <h3 className='text-3xl text-left font-semibold'>User Name</h3>
                     </div>
                 </div>
 
@@ -20,7 +20,7 @@ const Settings = () => {
 
                     {/* title div */}
                     <div className='w-1/2 '>
-                        <h3 className='text-2xl text-left'>General</h3>
+                        <h3 className='text-2xl text-left mb-3'>General</h3>
                         <hr></hr>
                     </div>
 
@@ -29,15 +29,11 @@ const Settings = () => {
                     <div className='mt-5 grid grid-cols-1 gap-3'>
 
                         {/* Name */}
-                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                            <input className='input input-text input-secondary' type="text" placeholder='Name' />
-                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        <form className='grid grid-cols-1 lg:grid-cols-6 gap-3'>
+                            <label className="flex items-center ">Name:</label>
+                            <input className='input input-text border-none col-span-5' type="text" placeholder='Name' />
                         </form>
-                        {/* postal code */}
-                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                            <input className='input input-number input-secondary' type="number" placeholder=' ' />
-                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
-                        </form>
+
                         {/* email subscription  */}
                         <div className='flex space-x-4'>
                             <input type={"checkbox"} className="checkbox-primary" /><p> Recieve Monthly Report</p>
@@ -60,7 +56,7 @@ const Settings = () => {
 
                     {/* title div */}
                     <div className='w-1/2 '>
-                        <h3 className='text-2xl text-left'>Address</h3>
+                        <h3 className='text-2xl text-left mb-3'>Address</h3>
                         <hr></hr>
                     </div>
 
@@ -69,15 +65,16 @@ const Settings = () => {
                     <div className='mt-5 grid grid-cols-1 gap-3'>
 
                         {/* address */}
-                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                            <input className='input input-text input-secondary' type="text" placeholder='address' />
-                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        <form className='grid grid-cols-1 lg:grid-cols-6 gap-3'>
+                            <label className="flex items-center ">Address:</label>
+                            <input className='input input-text border-none col-span-5' type="text" placeholder='City, Country' />
                         </form>
                         {/* postal code */}
-                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                            <input className='input input-number input-secondary' type="number" placeholder='postal code' />
-                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        <form className='grid grid-cols-1 lg:grid-cols-6 gap-3'>
+                            <label className="flex items-center ">Zip Code:</label>
+                            <input className='input input-text border-none col-span-5' type="number" placeholder='postal code' />
                         </form>
+
 
                     </div>
 
@@ -87,7 +84,7 @@ const Settings = () => {
                 <div className='rounded-xl p-3 lg:p-10 bg-secondary bg-opacity-25'>
                     {/* title div */}
                     <div className='w-1/2 '>
-                        <h3 className='text-2xl text-left'>Contact informations</h3>
+                        <h3 className='text-2xl text-left mb-3'>Contact informations</h3>
                         <hr></hr>
                     </div>
 
@@ -96,14 +93,15 @@ const Settings = () => {
                     <div className='mt-5 grid grid-cols-1 gap-3'>
 
                         {/* Email */}
-                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                            <input className='input input-email input-secondary' type="email" placeholder='E-mail Address' />
-                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+                        <form className='grid grid-cols-1 lg:grid-cols-6 gap-3'>
+                            <label className="flex items-center ">Email:</label>
+                            <input className='input input-text border-none col-span-5' type="email" placeholder='Email' />
                         </form>
                         {/* phone number */}
-                        <form className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                            <input className='input input-tel input-secondary' type={"tel"} placeholder='Phone Number' />
-                            <button type="submit" className='btn max-w-min btn-primary'>change</button>
+
+                        <form className='grid grid-cols-1 lg:grid-cols-6 gap-3'>
+                            <label className="flex items-center ">Phone :</label>
+                            <input className='input input-text border-none col-span-5' type={"tel"} placeholder='Phone Number' />
                         </form>
 
                     </div>
@@ -113,7 +111,7 @@ const Settings = () => {
                 <div className='rounded-xl p-3 lg:p-10 bg-secondary bg-opacity-25'>
                     {/* title div */}
                     <div className='w-1/2 '>
-                        <h3 className='text-2xl text-left'>Security</h3>
+                        <h3 className='text-2xl text-left mb-3'>Security</h3>
                         <hr />
                     </div>
 
@@ -121,15 +119,15 @@ const Settings = () => {
 
                     <form className='grid grid-cols-1 lg:grid-cols-2 gap-3 mt-5'>
                         {/* Current Password */}
-                        <input className='input input-password input-secondary' type="password" placeholder='Current Password' />
+                        <input className='input input-password ' type="password" placeholder='Current Password' />
 
-                        <input className='input input-password input-secondary' type="password" placeholder='New Password' />
+                        <input className='input input-password ' type="password" placeholder='New Password' />
                         <button type="submit" className='btn max-w-min btn-primary'>change</button>
                     </form>
                 </div>
 
                 {/* delete div */}
-                <div className='w-1/2 p-3 lg:p-10 '>
+                <div className='w-1/2 p-3 lg:p-10 bg-red-200 rounded-xl'>
                     <button type="submit" className='btn btn-outline btn-error ml-0 mb-3'>Delete Account</button>
                     <hr></hr>
                 </div>
