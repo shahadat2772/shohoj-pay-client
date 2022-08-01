68b3bcf5b461376b67038de9095cdaa50f84cd02
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-08-01 20:21:16 +0600

    api key problem fix

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
 };
-console.log(firebaseConfig);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
