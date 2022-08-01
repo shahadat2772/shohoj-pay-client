26ecafe27232d51a757e59335147574b564bb7b9
mahfuzswaron <mahfuzswaron@gmail.com>
2022-08-01 23:41:59 +0600

    icon styles updated

--- src/Components/Pages/Home/Sections/YouHavePower/YouHavePower.js
+++ src/Components/Pages/Home/Sections/YouHavePower/YouHavePower.js
@@ -14,7 +14,7 @@ import {
 const coreFeatures = () => {
   return (
     <div className="lg:p-20 md:p-18 py-20 w-full mx-auto bg-white">
-      <h2 className="text-3xl text-center font-bold font-sans text-primary bg-blue-50 p-10 rounded-full ">
+      <h2 className="text-3xl text-center font-bold font-sans text-primary">
         You have the power
       </h2>
       <p className="text-center mt-4">
@@ -25,75 +25,92 @@ const coreFeatures = () => {
       {/* CORE FUTURE CONTAINER */}
       <div className="coreServicesContainer mt-20 grid lg:grid-cols-4 md:grid-cols-2 gap-8">
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faMoneyBillTransfer}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faMoneyBillTransfer}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">Worldwide</p>
           <p>Worldwide payment is super easy with Shohoj Pay.</p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faMoneyCheck}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faMoneyCheck}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">eCheck</p>
           <p>Issue an eCheck with one tap with Shohoj Pay.</p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faPiggyBank}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faPiggyBank}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">Savings</p>
           <p>
-            Shohoj Pay also allows you to save money with upto 3% of interest.
+            Shohoj Pay offers you to save money with upto 3% of interest.
           </p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faMoneyBills}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faMoneyBills}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">Loan</p>
           <p>Shohoj Pay provides adequate amount.</p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faSackDollar}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faSackDollar}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">Less Cost</p>
           <p>
             Shohoj pay offers a little cost in transaction.
           </p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faBolt}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faBolt}
+            />
+          </figure>
+
           <p className="text-xl mt-4 mb-[6px]">Fast payment method</p>
           <p>Your time is more valuable to Shohoj Pay.</p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faShieldHalved}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faShieldHalved}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">Secured</p>
           <p>Shohoj Pay provides a strong secured system.</p>
         </div>
         <div className="coreService text-center shadow rounded-xl p-5 m-5 lg:m-0 md:m-0">
-          <FontAwesomeIcon
-            className="mx-auto text-4xl text-primary bg-blue-50 p-10 rounded-full "
-            icon={faHeadset}
-          />
+          <figure className="w-24 h-24 mx-auto bg-[#F7FAFC] rounded-full flex justify-center items-center">
+            <FontAwesomeIcon
+              className="mx-auto text-5xl text-primary"
+              icon={faHeadset}
+            />
+          </figure>
           <p className="text-xl mt-4 mb-[6px]">24/7 Support</p>
           <p>Shohoj Pay fight against your issues</p>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
