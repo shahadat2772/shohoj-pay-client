95a2bbf33edb8f355716fa1a0149d8606a52279a
solo-mizan <mizanbd.eee@gmail.com>
2022-08-02 07:31:03 +0600

    counterUp stat added

--- src/Components/Pages/Home/Sections/BottomSignUp/SignUp.js
+++ src/Components/Pages/Home/Sections/BottomSignUp/SignUp.js
@@ -4,7 +4,7 @@ import './SignUp.css';
 
 const SignUp = () => {
     return (
-        <div className="sign-up p-20 lg:p-28 text-center">
+        <div className="sign-up py-20 lg:py-28 px-4 text-center">
             <h1 className='text-white text-4xl lg:text-6xl pb-10 lg:pb-16'>Join the millions of users worldwide trusting Shohoj Pay everyday</h1>
             <Link to={'/signUp'} className='bg-white hover:bg-purple-300 text-xl py-2 px-12 rounded-full font-semibold'>Sign Up For Free</Link>
         </div>
--- src/Components/Shared/Spinner/Spinner.js
+++ src/Components/Shared/Spinner/Spinner.js
@@ -3,6 +3,7 @@ import { HashLoader } from "react-spinners";
 
 const Spinner = () => {
   return (
+    // hashLoader Loading spinner 
     <div className="flex items-center justify-center">
       <HashLoader color="#00ffd4" />
     </div>
