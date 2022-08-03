2a06e28d56c5a2c2728609e8a43931f32381e46a
zannatreya <zannatulbintabahar@yahoo.com>
2022-08-03 19:54:04 +0600

    updated

--- src/Components/Pages/Home/Sections/Header/Banner.js
+++ src/Components/Pages/Home/Sections/Header/Banner.js
@@ -4,12 +4,12 @@ import "./style.css";
 const Banner = () => {
     return (
         <>
-        <div class=" min-h-screen bg-img ">
+        <div class=" min-h-screen bg-img1">
   <div class="hero-overlay bg-opacity-30"></div>
   <div class="hero-content text-center text-neutral-content">
   <div className="max-w-lg">
             <h1 className="mb-5 mt-48 text-black text-4xl lg:text-5xl leading-10 font-bold ">
-              Empty Life Without <span className="block">Shohoj Pay</span>
+              Empty Life Without <span className="block mt-4">Shohoj Pay</span>
             </h1>
             <p className="text-black mt-4 mb-4 text-xl">
               We make your life easy by probiding you one of the most important
