cda34848f6211cc5d46285f8a7cbd6ffd8d03d78
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-08-03 13:08:59 +0600

    dashboard update

--- src/Components/Pages/Dashboard/Dashboard.js
+++ src/Components/Pages/Dashboard/Dashboard.js
@@ -4,49 +4,44 @@ import "./Dashboard.css";
 import auth from "../../../firebase.init";
 import { useAuthState } from "react-firebase-hooks/auth";
 import axios from "axios";
+import toast from "react-hot-toast";
 // USER TRANSACTION FAKE DATA
 const fakeTransaction = [
   {
     _id: 1,
+    date: "8/3/2022",
+    time: "11:40 PM",
     img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
     name: "User Name",
-    location: "Location",
+    actionType: "Send Money",
     money: 465,
   },
   {
     _id: 2,
+    date: "7/3/2022",
+    time: "10:40 PM",
     img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
     name: "User Name",
-    location: "Location",
+    actionType: "Add Money",
     money: 345,
   },
   {
     _id: 3,
+    time: "12:40 PM",
+    date: "6/3/2022",
     img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
     name: "User Name",
-    location: "Location",
+    actionType: "Send Money",
     money: 545,
   },
   {
     _id: 4,
+    date: "7/3/2022",
+    time: "12:40 PM",
     img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
     name: "User Name",
-    location: "Location",
+    actionType: "Add Money",
     money: 235,
-  },
-  {
-    _id: 5,
-    img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
-    name: "User Name",
-    location: "Location",
-    money: 955,
-  },
-  {
-    _id: 6,
-    img: "https://thumbs.dreamstime.com/z/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg",
-    name: "User Name",
-    location: "Location",
-    money: 713,
   },
 ];
 const COLORS = ["#000", "#414CDA", "#23E792", "#FF8042"];
@@ -90,18 +85,166 @@ let getDate =
 // WELCOME DASHBOARD SECTION
 const Dashboard = () => {
   const [balance, setBalance] = useState(0);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const todayDate = new Date().toLocaleDateString();
+  console.log(typeof date);
   console.log(balance);
   const [user] = useAuthState(auth);
   useEffect(() => {
     axios
       .get(`http://localhost:5000/getUserBalances/${user.email}`)
       .then((res) => setBalance(res.data));
-  }, [user.email]);
+    if (shareLinkCopied) {
+      toast.success("Copied Transaction Information");
+    }
+  }, [user.email, shareLinkCopied]);
+  // COPY TEXT FUNCTION
+  const onShare = (data) => {
+    navigator.clipboard.writeText(`
+    Date: ${data.date}
+    Name:${data.name}
+    Type: ${data.actionType}
+    Amount: ${data.money}$
+    `);
+    setShareLinkCopied(true);
+    setTimeout(() => {
+      setShareLinkCopied(false);
+    }, 2000);
+  };
+
   return (
-    <div className="container mx-auto mt-24 lg:mt-28 lg:px-10 py-10">
+    <div className="container mx-auto lg:mt-28 lg:px-10 py-10">
       {/* START USER INFORMATION AND TRANSACTION */}
       <div className="lg:flex">
+        {/* CARD DIVIDER HORIZONTAL */}
+
+        <div className="w-full mt-10 lg:mt-0">
+          <div class="md:mx-10 lg:mx-0 card lg:w-96 rounded ">
+            <div class="card-body py-0">
+              <h1 className="text-left text-3xl font-bold mb-3">
+                Hi, {user?.displayName}
+              </h1>
+              <div class="text-left">
+                <h4 className="">Total Balance</h4>
+                <h1 className="text-4xl font-bold">$ 245</h1>
+              </div>
+            </div>
+          </div>
+          {/* START SAVINGS MONTH */}
+          <div className="mt-10">
+            <div className=" px-2">
+              <h3 className="font-bold text-xl border-b-4 border-black pb-2 w-48">
+                Last Transaction
+              </h3>
+
+              <div className="mt-8">
+                <ul>
+                  {fakeTransaction.slice(0, 4).map((transAction) => (
+                    <li
+                      className={`flex items-center my-4 p-3 rounded-lg shadow-lg w-full ${
+                        transAction.actionType === "Add Money"
+                          ? "bg-green-200"
+                          : "bg-red-200"
+                      }`}
+                      key={transAction._id}
+                    >
+                      <div className="lg:mr-8 w-36">
+                        <h5>
+                          {transAction.date === todayDate
+                            ? "Today"
+                            : transAction.date}
+                        </h5>
+                        <h6>{transAction.time}</h6>
+                      </div>
+                      <div class="avatar">
+                        <div class="w-16 rounded-full ">
+                          <img src={transAction.img} alt="User Image" />
+                        </div>
+                      </div>
+                      <div className="ml-5 flex items-center justify-between w-full">
+                        <div>
+                          <h5
+                            className={`font-bold text-lg ${
+                              transAction.actionType === "Add Money"
+                                ? "text-green-800"
+                                : "text-red-800"
+                            }`}
+                          >
+                            {transAction.actionType}
+                          </h5>
+                          <h5 className="">{transAction.name}</h5>
+                        </div>
+                        <div className="" onClick={() => onShare(transAction)}>
+                          <i class="fa-solid fa-copy cursor-pointer"></i>
+                        </div>
+                        <div>
+                          <h3
+                            className={`text-lg font-bold text-right ${
+                              transAction.actionType === "Add Money"
+                                ? "text-green-800"
+                                : "text-red-800"
+                            }`}
+                          >
+                            {transAction.actionType === "Add Money"
+                              ? "+" + transAction.money
+                              : "-" + transAction.money}{" "}
+                            $
+                          </h3>
+                          <h6 style={{ fontSize: "14px" }}>
+                            TRX: lakshdfaoeeryhalkha45
+                          </h6>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                  <div className="text-center">
+                    <button className="btn btn-primary btn-sm mt-5 p-2">
+                      View All Transaction
+                    </button>
+                  </div>
+                </ul>
+              </div>
+            </div>
+            {/* START STATISTIC */}
+          </div>
+        </div>
+        <div class="divider divider-horizontal divide-black px-9 divider-hidden"></div>
+
         <div>
+          <div className="">
+            <div className="px-2 w-full">
+              <h3 class="font-bold text-xl pb-2 border-b border-black">
+                Statistic
+              </h3>
+              <h5 className="font-bold text-right text-xl">{getDate}</h5>
+              <div className="">
+                {/* <h4 className="font-bold text-2xl">Expense</h4> */}
+                <div className=" flex justify-center h-22">
+                  <PieChart width={290} height={330}>
+                    <Tooltip />
+                    <Legend style={{ width: "363px" }} />
+                    <Pie
+                      data={data}
+                      cx={120}
+                      cy={200}
+                      innerRadius={65}
+                      outerRadius={78}
+                      fill="#8884d8"
+                      paddingAngle={1}
+                      dataKey="value"
+                    >
+                      {data.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={COLORS[index % COLORS.length]}
+                        />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="md:mx-10 lg:mx-0 card lg:w-96 shadow-xl bg-primary text-white py-5 rounded card-1-bg">
             <div class="card-body">
               <h1 className="text-left text-4xl font-bold">
@@ -153,93 +296,6 @@ const Dashboard = () => {
                   </button>
                 </div>
               </ul>
-            </div>
-          </div>
-        </div>
-        {/* CARD DIVIDER HORIZONTAL */}
-        <div class="divider divider-horizontal divide-black px-9 divider-hidden"></div>
-        <div className="w-full mt-10 lg:mt-0">
-          <div class="card  shadow-xl rounded bg-image py-8 text-white">
-            <div class="card-body">
-              <h3 className="text-xl">Hi User!</h3>
-              <div className="mt-2">
-                <h3 className="font-bold text-xl">
-                  Have a Good Day With Shohoj Pay
-                </h3>
-                <h5>Thank You For Staying With Shohoj Pay.</h5>
-                <h5>We Are Always Working To Keep Your Account Safe.</h5>
-                <button className="btn btn-primary mt-4">Button</button>
-              </div>
-            </div>
-          </div>
-          {/* START SAVINGS MONTH */}
-          <div className="mt-10 md:flex lg:flex block">
-            <div className="lg:w-3/6 md:w-3/6 px-2">
-              <h3 className="font-bold text-xl border-b border-black pb-2">
-                Recent Month Savings
-              </h3>
-              <div className="mt-16">
-                <ul>
-                  {monthSavings.slice(0, 3).map((savings) => (
-                    <li
-                      className="flex items-center my-4 p-3 rounded-lg shadow-lg"
-                      key={savings._id}
-                    >
-                      <div class="avatar">
-                        <div class="w-16 rounded-full ">
-                          <img src={savings.img} alt="User Image" />
-                        </div>
-                      </div>
-                      <div className="ml-5 flex items-center justify-between w-full">
-                        <div>
-                          <h3 className="font-bold text-lg">{savings.month}</h3>
-                          <h5>{savings.year}</h5>
-                        </div>
-                        <div>
-                          <h3 className="text-lg font-bold">
-                            ${savings.money}
-                          </h3>
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            {/* START STATISTIC */}
-            <div className="lg:w-3/6 md:w-3/6 px-2 md:ml-8 lg:mt-0 md:mt-0 mt-10">
-              <div className="px-2">
-                <h3 class="font-bold text-xl pb-2 border-b border-black">
-                  Statistic
-                </h3>
-                <h5 className="font-bold text-right text-xl">{getDate}</h5>
-                <div className="">
-                  {/* <h4 className="font-bold text-2xl">Expense</h4> */}
-                  <div className=" flex justify-center">
-                    <PieChart width={290} height={330}>
-                      <Tooltip />
-                      <Legend style={{ width: "363px" }} />
-                      <Pie
-                        data={data}
-                        cx={120}
-                        cy={200}
-                        innerRadius={65}
-                        outerRadius={78}
-                        fill="#8884d8"
-                        paddingAngle={1}
-                        dataKey="value"
-                      >
-                        {data.map((entry, index) => (
-                          <Cell
-                            key={`cell-${index}`}
-                            fill={COLORS[index % COLORS.length]}
-                          />
-                        ))}
-                      </Pie>
-                    </PieChart>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
