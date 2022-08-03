091e96f7cb4759d2c72016716e0fc1ff47b74684
solo-mizan <mizanbd.eee@gmail.com>
2022-08-03 18:21:02 +0600

    countUp stats added

--- src/Components/Pages/Home/Sections/BottomSignUp/SignUp.js
+++ src/Components/Pages/Home/Sections/BottomSignUp/SignUp.js
@@ -5,7 +5,7 @@ import './SignUp.css';
 const SignUp = () => {
     return (
         <div className="sign-up">
-            <h1 className='text-white text-4xl lg:text-6xl p-6 lg:p-16'>Join the millions of users worldwide trusting Shohoj Pay everyday</h1>
+            <h1 className='text-white text-4xl lg:text-6xl p-6 lg:p-16 text-'>Join the millions of users worldwide trusting Shohoj Pay everyday</h1>
             <Link to={'/signUp'} className='bg-white hover:bg-purple-200 text-xl py-2 px-12 rounded-full font-semibold'>Sign Up For Free</Link>
         </div>
     );
