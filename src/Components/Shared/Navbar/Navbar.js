ac794c8fc2dcdef83df8b84058252e0641c1d896
solo-mizan <mizanbd.eee@gmail.com>
2022-08-03 13:36:37 +0600

    navbar hide and show on scroll functionality added

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import { MenuIcon, XIcon } from "@heroicons/react/solid";
 import { Link, useNavigate } from "react-router-dom";
@@ -18,6 +18,34 @@ const Navbar = () => {
     { name: "Settings", link: "/settings" },
   ];
 
+  // on scroll hide and show navbar functionality 
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+        setShow(true);
+      } else { // if scroll up show the navbar
+        setShow(false);
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   // RESPONSIVE TOGGLER BTN STATE
   const [open, setOpen] = useState(false);
   // ONCLICK NAVIGATE
@@ -32,47 +60,72 @@ const Navbar = () => {
     toast.success("Sign Out Successfully");
   };
   return (
-    <div className="fixed top-0 w-[100%] z-50">
-      <div className="nav-active px-4 py-2 lg:rounded-2xl lg:p-0 lg:m-4 lg:mt-2">
-        <div className="p-1 lg:px-8 md:px-4">
-          <nav className="flex items-center justify-between">
-            {/* PROJECT LOGO */}
-            <div>
-              <Link to="/">
-                <img
-                  src="/assets/logos/logo-horizontal.png"
-                  className="w-40"
-                  alt=""
-                />
-                {/* {logo} */}
-              </Link>
-            </div>
-            <div className="ml-20">
-              {" "}
-              {/* NAV ITEM */}
-              <ul
-                className={`lg:flex w-100 h-72 lg:h-auto lg:w-full block lg:items-center navbar absolute duration-500 ease-in lg:static top-16 lg:bg-transparent bg-white overflow-hidden ${
-                  open ? "left-[-10px] top-16" : "left-[-1080px]"
-                }`}
-              >
-                {link.map((item) => (
-                  <li className="block text-center">
-                    <Link to={item.link}>{item.name}</Link>
-                  </li>
-                ))}
-                {/* Routes for authenticated users   */}
-                {user &&
-                  restrictedLinks.map((item) => (
+    <nav className={`active ${show && 'hidden'}`}>
+      <div className="fixed top-0 w-[100%] z-50">
+        <div className="nav-active px-4 py-2 lg:rounded-2xl lg:p-0 lg:m-4 lg:mt-2">
+          <div className="p-1 lg:px-8 md:px-4">
+            <nav className="flex items-center justify-between">
+              {/* PROJECT LOGO */}
+              <div>
+                <Link to="/">
+                  <img
+                    src="/assets/logos/logo-horizontal.png"
+                    className="w-40"
+                    alt=""
+                  />
+                  {/* {logo} */}
+                </Link>
+              </div>
+              <div className="ml-20">
+                {" "}
+                {/* NAV ITEM */}
+                <ul
+                  className={`lg:flex w-100 h-72 lg:h-auto lg:w-full block lg:items-center navbar absolute duration-500 ease-in lg:static top-16 lg:bg-transparent bg-white overflow-hidden ${open ? "left-[-10px] top-16" : "left-[-1080px]"
+                    }`}
+                >
+                  {link.map((item) => (
                     <li className="block text-center">
                       <Link to={item.link}>{item.name}</Link>
                     </li>
                   ))}
-                {/* RESPONSIVE LOGIN OR SIGN UP  BUTTON */}
-                <div className=" flex items-center justify-center lg:hidden">
+                  {/* Routes for authenticated users   */}
+                  {user &&
+                    restrictedLinks.map((item) => (
+                      <li className="block text-center">
+                        <Link to={item.link}>{item.name}</Link>
+                      </li>
+                    ))}
+                  {/* RESPONSIVE LOGIN OR SIGN UP  BUTTON */}
+                  <div className=" flex items-center justify-center lg:hidden">
+                    {user ? (
+                      <button
+                        onClick={handleSignOut}
+                        className="btn hover:bg-transparent border-2 hover:text-secondary border-secondary hover:border-secondary bg-secondary  text-white btn-sm lg:mx-4 "
+                      >
+                        Sign Out
+                      </button>
+                    ) : (
+                      <>
+                        <button
+                          onClick={handleNavigateLogin}
+                          className="btn bg-transparent border-2 text-secondary hover:border-secondary hover:bg-secondary border-secondary hover:text-white btn-sm mr-3"
+                        >
+                          Login
+                        </button>
+                        <button
+                          onClick={handleNavigateSignUp}
+                          className=" btn bg-[#3F4AD9] hover:bg-primary border-0 btn-sm"
+                        >
+                          Sign Up
+                        </button>
+                      </>
+                    )}
+                  </div>
+                  {/* LARGE DEVICE LOGIN OR SIGN UP BUTTON */}
                   {user ? (
                     <button
                       onClick={handleSignOut}
-                      className="btn hover:bg-transparent border-2 hover:text-secondary border-secondary hover:border-secondary bg-secondary  text-white btn-sm lg:mx-4 "
+                      className="btn hover:bg-transparent border-2 hover:text-secondary border-secondary hover:border-secondary bg-secondary  text-white btn-sm lg:mx-4 hidden lg:block"
                     >
                       Sign Out
                     </button>
@@ -80,53 +133,29 @@ const Navbar = () => {
                     <>
                       <button
                         onClick={handleNavigateLogin}
-                        className="btn bg-transparent border-2 text-secondary hover:border-secondary hover:bg-secondary border-secondary hover:text-white btn-sm mr-3"
+                        className="btn bg-transparent border-2 text-secondary hover:border-secondary hover:bg-secondary border-secondary hover:text-white btn-sm lg:mx-4 hidden lg:block"
                       >
                         Login
                       </button>
                       <button
                         onClick={handleNavigateSignUp}
-                        className=" btn bg-[#3F4AD9] hover:bg-primary border-0 btn-sm"
+                        className="btn bg-primary hover:bg-[#1d27af] text-white border-0 btn-sm hidden lg:block"
                       >
                         Sign Up
                       </button>
                     </>
                   )}
-                </div>
-                {/* LARGE DEVICE LOGIN OR SIGN UP BUTTON */}
-                {user ? (
-                  <button
-                    onClick={handleSignOut}
-                    className="btn hover:bg-transparent border-2 hover:text-secondary border-secondary hover:border-secondary bg-secondary  text-white btn-sm lg:mx-4 hidden lg:block"
-                  >
-                    Sign Out
-                  </button>
-                ) : (
-                  <>
-                    <button
-                      onClick={handleNavigateLogin}
-                      className="btn bg-transparent border-2 text-secondary hover:border-secondary hover:bg-secondary border-secondary hover:text-white btn-sm lg:mx-4 hidden lg:block"
-                    >
-                      Login
-                    </button>
-                    <button
-                      onClick={handleNavigateSignUp}
-                      className="btn bg-primary hover:bg-[#1d27af] text-white border-0 btn-sm hidden lg:block"
-                    >
-                      Sign Up
-                    </button>
-                  </>
-                )}
-              </ul>
-            </div>
-            {/* NAVBAR TOGGLER ICON */}
-            <div onClick={() => setOpen(!open)} className="w-10 h-10 lg:hidden">
-              {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
-            </div>
-          </nav>
+                </ul>
+              </div>
+              {/* NAVBAR TOGGLER ICON */}
+              <div onClick={() => setOpen(!open)} className="w-10 h-10 lg:hidden">
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+              </div>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
