d66b127d2d296fa1763cb71adcb2604247c92654
mahfuzswaron <mahfuzswaron@gmail.com>
2022-08-03 19:44:52 +0600

    redux is ready to set user

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,22 +1,15 @@
 import { ActionTypes } from "../constants/actionTypes";
 
 const initialState = {
-    user: { name: "mahfuz swaron" }
-}
-const setUser = (user, state) => {
-    const newState = { ...state };
-    newState.user = user;
-    console.log(user)
-    return newState;
+    user: {}
 }
 
-export const userReducer = (state = initialState, type, payload) => {
-    // switch (type) {
-    //     case ActionTypes.SET_USER:
-    //         return setUser(payload, state)
+export const userReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SET_USER:
+            return { ...payload }
 
-    //     default:
-    //         return state;
-    // }
-    return { user: payload }
+        default:
+            return state;
+    }
 }
