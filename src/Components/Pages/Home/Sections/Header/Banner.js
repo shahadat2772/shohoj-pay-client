bfbf803f1e69cb575a2abd5ba886fd661cab6a74
zannatreya <zannatulbintabahar@yahoo.com>
2022-08-04 17:25:12 +0600

    updated

--- src/Components/Pages/Home/Sections/Header/Banner.js
+++ src/Components/Pages/Home/Sections/Header/Banner.js
@@ -8,15 +8,15 @@ const Banner = () => {
   <div class="hero-overlay bg-opacity-30"></div>
   <div class="hero-content text-center text-neutral-content">
   <div className="max-w-lg">
-            <h1 className="mb-5 mt-48 text-black text-4xl lg:text-5xl leading-10 font-bold ">
-              Empty Life Without <span className="block mt-4">Shohoj Pay</span>
+            <h1 className="mb-4 mt-40 font-mono text-zinc-800 text-4xl lg:text-5xl leading-10 font-bold ">
+              Empty Life Without <span className="block mt-4 font-mono">Shohoj Pay</span>
             </h1>
-            <p className="text-black mt-4 mb-4 text-xl">
-              We make your life easy by probiding you one of the most important
+            <p className="text-slate-800 mt-4 mb-2 text-xl font-serif">
+              We make your life easy by providing most important
               services that is Finance. Use Shohoj Pay and kick out your
               problems.
             </p>
-            <button class="text-black bg-white font-bold text-2xl outline outline-4  outline-offset-2 outline-base-300 border-4 rounded-full border-base-300 px-8 py-2 mt-4 hover:bg-secondary hover:text-white">Get Free 25$</button>
+            <button class="text-black font-mono bg-white font-bold text-2xl outline outline-4  outline-offset-2 outline-base-300 border-4 rounded-full border-base-300 px-8 py-2 mt-4 hover:bg-secondary hover:text-white">Get Free 25$</button>
           </div>
   </div>
 </div>
