b9ef3df8244dd4f11227227cbba40989a122fc62
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-08-21 20:05:34 +0600

    eCheck implement done

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import Spinner from "./Components/Shared/Spinner/Spinner";
 import RestrictAuth from "./Components/Pages/Authentication/RestrictAuth/RestrictAuth";
 import axios from "axios";
 import Notification from "./Components/Pages/Notificaion/Notification";
+import ECheck from "./Components/Pages/Services/ECheck/ECheck";
 function App() {
   // State for confirming the money request
   const [requestForConfirm, setRequestForConfirm] = useState([]);
@@ -135,6 +136,16 @@ function App() {
             <RequireAuth>
               <RequirePersonal>
                 <SendMoney />
+              </RequirePersonal>
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/services/eCheck"
+          element={
+            <RequireAuth>
+              <RequirePersonal>
+                <ECheck />
               </RequirePersonal>
             </RequireAuth>
           }
