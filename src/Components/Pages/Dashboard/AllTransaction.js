2e0a491cc8df317216b7a0f101f3a3d2f8ff9d62
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-08-24 12:24:48 +0600

    pagination added done

--- src/Components/Pages/Dashboard/AllTransaction.js
+++ src/Components/Pages/Dashboard/AllTransaction.js
@@ -1,46 +1,56 @@
-import axios from "axios";
 import { signOut } from "firebase/auth";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUserEmailInfo } from "../../../app/features/userAllEmailInfoSlice";
+import { useNavigate } from "react-router-dom";
+import { fetchAllTransaction } from "../../../app/features/transAction/transactionSlice";
 import auth from "../../../firebase.init";
 import Spinner from "../../Shared/Spinner/Spinner";
 import "./AllTransaction.css";
+import Pagination from "../../Shared/Pagination/Pagination";
 
 const AllTransaction = () => {
   const [filterData, setFilterData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
-  // const navigate = useNavigate();
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
   const todayDate = new Date().toLocaleDateString();
   // REVERSE TRANSACTION DATA
   const reverseData = [...filterData].reverse();
   const dispatch = useDispatch();
-  const { isLoading, allInfo, error } = useSelector(
-    (state) => state.userAllEmailData
+  const { isLoading, allTransactionData, error } = useSelector(
+    (state) => state.allTransaction
   );
-  console.log(reverseData);
   useEffect(() => {
-    dispatch(fetchUserEmailInfo(user));
+    dispatch(fetchAllTransaction(user));
   }, [dispatch, user]);
-  const transactionData = allInfo?.userTransactionInfo;
+  const transactionData = allTransactionData;
+  console.log(transactionData);
   useEffect(() => {
-    // setFilterData(allInfo?.userTransactionInfo);
+    setFilterData(allTransactionData);
     if (shareLinkCopied) {
       toast.success("Copied Transaction Information");
     }
-  }, [shareLinkCopied, allInfo]);
-  if (isLoading || transactionData == undefined) {
+  }, [shareLinkCopied, allTransactionData]);
+  // PAGINATION
+  let PageSize = 10;
+  const currentTrxData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return reverseData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, reverseData]);
+  if (isLoading) {
     return <Spinner />;
   }
   if (error) {
-    return toast.error(error?.message);
+    localStorage.removeItem("accessToken");
+    signOut(auth);
+    toast.error(error?.message);
+    navigate("/");
   }
-  // setTimeout(() => {
-  //   setFilterData(transactionData);
-  // });
+
   // FILTER TRANSACTION DATA
   const handleFilterMonth = (e) => {
     const getMonth = transactionData.filter((data) =>
@@ -134,7 +144,7 @@ const AllTransaction = () => {
         </div>
         <div className="mt-8">
           <ul>
-            {reverseData.map((transAction) => (
+            {currentTrxData.map((transAction) => (
               <li
                 className={`flex items-center my-4 p-3 rounded-lg w-full shadow`}
                 key={transAction._id}
@@ -148,7 +158,7 @@ const AllTransaction = () => {
                   <h6 className="gray md-responsive">{transAction.time}</h6>
                 </div>
                 <div className="avatar">
-                  <div className="w-16 md-img-responsive rounded-full ">
+                  <div className="w-14 md-img-responsive rounded-full ">
                     <img
                       src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png"
                       alt="User Image"
@@ -160,28 +170,21 @@ const AllTransaction = () => {
                     <h5 className="font-bold text-lg md-type-responsive ">
                       {transAction?.type}
                     </h5>
-                    <h5 className="gray md-responsive">
+                    <h6 className="gray md-responsive">
                       {transAction?.userName}
-                    </h5>
+                    </h6>
                     {transAction?.transactionId && (
                       <h6 className="gray md-trx-responsive">
                         {transAction.transactionId}
                       </h6>
                     )}
-                    {transAction?.type === "E-Check" && (
-                      <h6 className="gray md-trx-responsive">
-                        <span className="font-bold text-black">Reference:</span>{" "}
-                        {transAction?.reference}
-                      </h6>
-                    )}
-                    <h5 className="gray md-responsive">{transAction?.email}</h5>
                   </div>
-                  <div className="" onClick={() => onShare(transAction)}>
-                    <i className="fa-solid fa-copy cursor-pointer"></i>
-                  </div>
-                  <div>
+                  <div className="flex align-items-center ">
+                    <div className="" onClick={() => onShare(transAction)}>
+                      <i className="fa-solid fa-copy cursor-pointer"></i>
+                    </div>
                     <h3
-                      className={`text-2xl font-medium  text-right md-amount-responsive ${
+                      className={`text-2xl amount-style font-medium  text-right md-amount-responsive ${
                         transAction.type === "Add Money" ||
                         transAction.type === "Receive Money"
                           ? "text-green-600"
@@ -200,20 +203,15 @@ const AllTransaction = () => {
             ))}
           </ul>
         </div>
-        {/* <ReactPaginate
-          pageCount={pageCount}
-          breakLabel="..."
-          nextLabel="next >"
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={3}
-          previousLabel="< previous"
-          renderOnZeroPageCount={null}
-          containerClassName="pagination"
-          pageLinkClassName="page-name"
-          previousLinkClassName="page-num"
-          nextLinkClassName="page-num"
-          activeLinkClassName="active"
-        /> */}
+        <div className={`mt-12 ${allTransactionData.length < 10 && "hidden"}`}>
+          <Pagination
+            className="pagination-bar"
+            currentPage={currentPage}
+            totalCount={filterData.length}
+            pageSize={PageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+        </div>
       </div>
     </div>
   );
