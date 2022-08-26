6b2b5418e72a62b0c1de9e3c2fa02f4aa39e9008
mahfuz swaron <mahfuzswaron@gmail.com>
2022-08-25 22:18:16 -0400

    services section done

--- src/Components/Pages/Merchant/MerchantDashboard/MerchantDashboard.js
+++ src/Components/Pages/Merchant/MerchantDashboard/MerchantDashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BalanceSection from './BalanceSection';
+import ServicesSection from './ServicesSection';
 
 const MerchantDashboard = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-12 mt-32 lg:px-20'>
+        <div className='grid grid-cols-1 lg:grid-cols-12 py-32 lg:px-20 bg-white'>
             {/* left part */}
-            <div className='lg:col-span-7'>
+            <div className='lg:col-span-7 grid grid-cols-1 gap-5'>
                 <BalanceSection />
+                <ServicesSection />
             </div>
 
             {/* right part  */}
