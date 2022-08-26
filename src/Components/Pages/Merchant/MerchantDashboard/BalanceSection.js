d0d4b1d1b771c80e3d0ec63ac4c98fe63ae98d8d
mahfuz swaron <mahfuzswaron@gmail.com>
2022-08-26 02:17:17 -0400

    date maked dynamic

--- src/Components/Pages/Merchant/MerchantDashboard/BalanceSection.js
+++ src/Components/Pages/Merchant/MerchantDashboard/BalanceSection.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 const BalanceSection = ({ balance }) => {
-
+    const today = new Date().toLocaleDateString("en-us", {
+        dateStyle: "medium",
+    });
     return (
         <div className='bg-blue-100 rounded-lg w-full p-10'>
             <h4 className='text-2xl'>Your Balance </h4>
             <h2 className='text-6xl font-bold my-7'>${balance}</h2>
-            <p className=''>26 August, 2022</p>
+            <p className=''>{today} </p>
         </div>
     );
 };
