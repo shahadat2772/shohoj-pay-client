580c73d5bce46610746e7959fb037621a4cdad43
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-08-28 22:38:42 +0600

    conflig error colved

--- src/app/store.js
+++ src/app/store.js
@@ -2,22 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import allTransactionReducer from "./slices/transactionSlice";
 import userAllEmailData from "./slices/userAllEmailInfoSlice";
 import signUpLoadingReducer from "./slices/signUpLoadingSlice";
-<<<<<<< HEAD
-=======
-import allServiceData from "./features/getAllServiceSlice";
+// import allServiceData from "./slices/getAllServiceSlice";
 import notificationReducer from "./slices/notificationSlice";
->>>>>>> 89446ec7e9d830ad728647cae6b2fcff192701f8
 
 const store = configureStore({
   reducer: {
     allTransaction: allTransactionReducer,
     userAllEmailData: userAllEmailData,
     signUpLoading: signUpLoadingReducer,
-<<<<<<< HEAD
-=======
-    getAllService: allServiceData,
+    // getAllService: allServiceData,
     allNotification: notificationReducer,
->>>>>>> 89446ec7e9d830ad728647cae6b2fcff192701f8
   },
 });
 export default store;
