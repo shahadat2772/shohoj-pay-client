67fbf4f896cd968e03981e691e400a908cfa9fd5
mahfuz swaron <mahfuzswaron@gmail.com>
2022-09-01 09:13:46 -0400

    settings page maked responsive

--- src/Components/Pages/Settings/Settings.js
+++ src/Components/Pages/Settings/Settings.js
@@ -15,7 +15,7 @@ const Settings = () => {
   const [firebaseUser, loading] = useAuthState(auth);
   const [userName, setUserName] = useState(user?.name);
   const [userAddress, setUserAddress] = useState(user?.address);
-  const [userEmail, setUserEmail] = useState(user?.email);
+  const [userEmail,] = useState(user?.email);
   const [userPhone, setUserPhone] = useState(user?.phone);
   const [nameCanSave, setNameCanSave] = useState(false);
   const [AddressCanSave, setAddressCanSave] = useState(false);
@@ -96,21 +96,16 @@ const Settings = () => {
         <div className="rounded-lg p-5 w-full lg:w-10/12 bg-white relative">
           <div className="w-full flex-col items-center ">
             <div>
-              <figure className="flex justify-start items-end mb-3 relative ">
+              <figure className="flex justify-start items-end  relative ">
                 <img src={updatedImg || user?.avatar} alt="profile " className="h-44 w-44 mask mask-circle" />
-                <label htmlFor="imgInput" className={`${editName ? " cursor-pointer p-1 flex justify-center items-center bg-base-100 relative right-12 rounded-full" : "hidden"}`}>
+                <label htmlFor="imgInput" title="upload new picture" className={`${editName ? " cursor-pointer p-1 flex justify-center items-center bg-base-100 relative right-12 rounded-full" : "hidden"}`}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
                     <path d="M12 9a3.75 3.75 0 100 7.5A3.75 3.75 0 0012 9z" />
                     <path fillRule="evenodd" d="M9.344 3.071a49.52 49.52 0 015.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 01-3 3h-15a3 3 0 01-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 001.11-.71l.822-1.315a2.942 2.942 0 012.332-1.39zM6.75 12.75a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0zm12-1.5a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
                   </svg>
                 </label>
                 <input className="hidden" id="imgInput" type={"file"} accept="image/*" onChange={(e) => uploadImg(e)} />
-                {/* <div
-                  className={` text-white text-center font-semibold absolute h-24 w-44 bg-black opacity-0 hover:bg-opacity-25 hover:opacity-100 pt-5 rounded-br-full rounded-bl-full flex justify-center items-center
-                  
-                  `}
-                >
-                </div> */}
+
               </figure>
             </div>
             <div className="w-full">
@@ -127,11 +122,11 @@ const Settings = () => {
               />
             </div>
           </div>
-          <div className="absolute top-3 right-3 flex items-center space-x-4">
+          <div className="absolute top-4 right-4 flex items-center justify-around">
             <div
               onClick={() => setEditName(true)}
               className={`${editName && "hidden"
-                } cursor-pointer col-span-3 place-self-center`}
+                } cursor-pointer place-self-center`}
             >
               {/* edit  */}
               <FontAwesomeIcon className=" text-gray-500" icon={faPen} />
@@ -142,7 +137,7 @@ const Settings = () => {
                 setUpdatedImg(user?.avatar)
               }}
               className={`${!editName && "hidden"
-                } cursor-pointer col-span-3 px-4 py-2 rounded-lg place-self-center`}
+                } cursor-pointer px-4 py-2 rounded-lg place-self-center`}
             >
               {/* cancel  */}
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -178,7 +173,7 @@ const Settings = () => {
         <div className="rounded-lg p-5 w-full lg:w-10/12 place-self-end  mr-0 mt-5 bg-white ">
           {/* title div */}
           <div className="w-1/2 ">
-            <h3 className="text-xl text-left mb-3">Security</h3>
+            <h3 className="text-xl text-left ">Security</h3>
           </div>
           <p
             onClick={() => navigate("/resetPassword")}
@@ -195,13 +190,13 @@ const Settings = () => {
         <div className="rounded-lg p-5 w-full lg:w-10/12 place-self-end  bg-white ">
           {/* title div */}
           <div className="flex justify-between items-center relative">
-            <h3 className="text-xl text-left mb-3">Address</h3>
+            <h3 className="text-xl text-left ">Address</h3>
 
-            <div className="absolute top-3 right-3 flex items-center space-x-4">
+            <div className=" flex items-center justify-around">
               <div
                 onClick={() => setEditAddress(true)}
                 className={`${editAddress && "hidden"
-                  } cursor-pointer col-span-3 place-self-center`}
+                  } cursor-pointer place-self-center`}
               >
                 {/* edit  */}
                 <FontAwesomeIcon className=" text-gray-500" icon={faPen} />
@@ -209,7 +204,7 @@ const Settings = () => {
               <div
                 onClick={() => setEditAddress(false)}
                 className={`${!editAddress && "hidden"
-                  } cursor-pointer col-span-3 px-4 py-2 rounded-lg place-self-center`}
+                  } cursor-pointer px-4 py-2 rounded-lg place-self-center`}
               >
                 {/* cancel  */}
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -259,13 +254,13 @@ const Settings = () => {
         <div className="rounded-lg p-5 w-full lg:w-10/12 place-self-end  mr-0 bg-white ">
           {/* title div */}
           <div className="flex justify-between items-center relative">
-            <h3 className="text-xl text-left mb-3">Contact informations</h3>
+            <h3 className="text-xl text-left ">Contact</h3>
 
-            <div className="absolute top-3 right-3 flex items-center space-x-4">
+            <div className=" flex items-center justify-around">
               <div
                 onClick={() => setEditContact(true)}
                 className={`${editContact && "hidden"
-                  } cursor-pointer col-span-3 place-self-center`}
+                  } cursor-pointer place-self-center`}
               >
                 {/* edit  */}
                 <FontAwesomeIcon className=" text-gray-500" icon={faPen} />
@@ -273,7 +268,7 @@ const Settings = () => {
               <div
                 onClick={() => setEditContact(false)}
                 className={`${!editContact && "hidden"
-                  } cursor-pointer col-span-3 px-4 py-2 rounded-lg place-self-center`}
+                  } cursor-pointer px-4 py-2 rounded-lg place-self-center`}
               >
                 {/* cancel  */}
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
