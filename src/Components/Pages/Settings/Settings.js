748350f6a4e034d35f8b4bf27947a7fe47d19940
mahfuz swaron <mahfuzswaron@gmail.com>
2022-09-01 08:55:14 -0400

    controlled displaying img btn

--- src/Components/Pages/Settings/Settings.js
+++ src/Components/Pages/Settings/Settings.js
@@ -98,23 +98,19 @@ const Settings = () => {
             <div>
               <figure className="flex justify-start items-end mb-3 relative ">
                 <img src={updatedImg || user?.avatar} alt="profile " className="h-44 w-44 mask mask-circle" />
-
-                <div
+                <label htmlFor="imgInput" className={`${editName ? " cursor-pointer p-1 flex justify-center items-center bg-base-100 relative right-12 rounded-full" : "hidden"}`}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
+                    <path d="M12 9a3.75 3.75 0 100 7.5A3.75 3.75 0 0012 9z" />
+                    <path fillRule="evenodd" d="M9.344 3.071a49.52 49.52 0 015.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 01-3 3h-15a3 3 0 01-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 001.11-.71l.822-1.315a2.942 2.942 0 012.332-1.39zM6.75 12.75a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0zm12-1.5a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+                  </svg>
+                </label>
+                <input className="hidden" id="imgInput" type={"file"} accept="image/*" onChange={(e) => uploadImg(e)} />
+                {/* <div
                   className={` text-white text-center font-semibold absolute h-24 w-44 bg-black opacity-0 hover:bg-opacity-25 hover:opacity-100 pt-5 rounded-br-full rounded-bl-full flex justify-center items-center
                   
                   `}
                 >
-                  <label hidden={!editName} htmlFor="imgInput" className="cursor-pointer p-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
-                      <path d="M12 9a3.75 3.75 0 100 7.5A3.75 3.75 0 0012 9z" />
-                      <path fillRule="evenodd" d="M9.344 3.071a49.52 49.52 0 015.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 01-3 3h-15a3 3 0 01-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 001.11-.71l.822-1.315a2.942 2.942 0 012.332-1.39zM6.75 12.75a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0zm12-1.5a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
-                    </svg>
-                  </label>
-                  <input className="hidden" id="imgInput" type={"file"} accept="image/*" onChange={(e) => uploadImg(e)} />
-
-
-
-                </div>
+                </div> */}
               </figure>
             </div>
             <div className="w-full">
