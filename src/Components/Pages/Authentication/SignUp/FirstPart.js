dfe9057539bef47d3b7885e41ddb39cd1bc113d4
mahfuz swaron <mahfuzswaron@gmail.com>
2022-09-02 02:54:29 -0400

    selecting city done

--- src/Components/Pages/Authentication/SignUp/FirstPart.js
+++ src/Components/Pages/Authentication/SignUp/FirstPart.js
@@ -1,3 +1,4 @@
+import { SafetyCertificateTwoTone } from '@ant-design/icons';
 import { async } from '@firebase/util';
 import { isAsyncThunkAction } from '@reduxjs/toolkit';
 import React, { useEffect, useState } from 'react';
@@ -13,6 +14,7 @@ const FirstPart = ({
     const [data, setData] = useState([]);
     const [countries, setCountries] = useState([]);
     const [selectedCountry, setSelectedCountry] = useState("");
+    const [cities, setCities] = useState([]);
     const fetchData = async () => {
         try {
             const response = await fetch("https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json");
@@ -20,7 +22,7 @@ const FirstPart = ({
             if (jsonData) {
                 setData(await jsonData);
                 const uniqueData = [...new Set(data.map((item) => item.country))];
-                setCountries(uniqueData);
+                setCountries(uniqueData.sort());
             }
         }
         catch (e) {
@@ -29,12 +31,11 @@ const FirstPart = ({
     }
     useEffect(() => {
         fetchData();
-        // console.log(data);
-    }, []);
+        const cities = data.filter(c => c.country === selectedCountry);
+        setCities(cities.map(c => c.name).sort());
+    }, [data]);
 
-
-    console.log(selectedCountry);
-
+    // console.log(cities.length)
     return (
         <div className={`${showNamePart ? "block" : "hidden"}`}>
             <div className="form-control w-full  ">
@@ -94,29 +95,7 @@ const FirstPart = ({
                     )}
                 </label>
             </div>
-            <div className="form-control w-full  ">
-                <label className="label">
-                    <span className="label-name">Address</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="city, country"
-                    className="input input-bordered w-full   "
-                    {...register("address", {
-                        required: {
-                            value: true,
-                            message: "Address is Required",
-                        },
-                    })}
-                />
-                <label className="label">
-                    {errors.address?.type === "required" && (
-                        <span className="label-text-alt text-red-500">
-                            {errors.address.message}
-                        </span>
-                    )}
-                </label>
-            </div>
+
 
             <div className='lg:grid grid-cols-2 gap-5 mb-5'>
                 <div className="form-control w-full ">
@@ -156,7 +135,11 @@ const FirstPart = ({
                         })}
                         className='select select-bordered'>
                         {
-                            <option>{"dhaka"} </option>
+                            cities.map((c) => (
+                                <option key={c} value={c} >
+                                    {c}
+                                </option>
+                            ))
                         }
                     </select>
                 </div>
@@ -167,7 +150,8 @@ const FirstPart = ({
                     if (
                         errors &&
                         !errors.name &&
-                        !errors.address &&
+                        !errors.country &&
+                        !errors.city &&
                         !errors.email
                     ) {
                         setProgress(2);
